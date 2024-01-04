f1b0d93a326589c1285fe08580176988cee52dea
Subhajit <shubhajit.pramanik@edudigm.in>
2024-01-03 22:16:56 -1200

    Code commited for react tooltip

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Tooltip from "./Tooltip";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div style={{ display: "flex", justifyContent: "center", padding: "20%" }}>
+      <Tooltip position="top" text="Top Tooltip">
+        <button className="hover-btn">Hover me</button>
+      </Tooltip>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Tooltip.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import "./Tooltip.css"; // You can style it as you like
+
+const Tooltip = ({ position, text, children }) => {
+  const [showTooltip, setShowTooltip] = useState(false);
+
+
+  const getTooltipStyle = () => {
+    switch (position) {
+      case "top":
+        return { bottom: "100%", left: "50%", transform: "translateX(-50%)" };
+      case "bottom":
+        return { top: "100%", left: "50%", transform: "translateX(-50%)" };
+      case "right":
+        return { top: "50%", left: "100%", transform: "translateY(-50%)" };
+      case "left":
+        return { top: "50%", right: "100%", transform: "translateY(-50%)" };
+      default:
+        return {};
+    }
+  };
+
+  return (
+    <div
+      className="tooltip-container"
+      onMouseEnter={() => setShowTooltip(true)}
+      onMouseLeave={() => setShowTooltip(false)}
+    >
+      {children}
+      {showTooltip && (
+        <div
+          className={`tooltip tooltip-${position}`}
+          style={getTooltipStyle()}
+        >
+          {text}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Tooltip;
